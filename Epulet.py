26c9d52ab0100d61a5e7970bbaf1e9330791df39
MGergo20 <medgyaszayg3@gmail.com>
2024-12-19 08:41:49 +0100

    Befejezve

---
+++ Epulet.py
@@ -0,0 +1,8 @@
+class Epulet:
+    def __init__(self, nev, varos, orszag, magassag, emeletek_szama, epites_eve):
+        self.nev = nev
+        self.varos = varos
+        self.orszag = orszag
+        self.magassag = str(magassag)
+        self.emeletek_szama = int(emeletek_szama)
+        self.epites_eve = int(epites_eve)
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import bevezetes, sorozat
+import bevezetes, sorozat, epuletek, Epulet
 
 
-bevezetes.adatbekeres()
+"""bevezetes.adatbekeres()
 
 fejek=sorozat.fejek_szama()
 
 sorozat.konzol_kiir(fejek)
 
-sorozat.file_kiir(fejek)
+sorozat.file_kiir(fejek)"""
+
+
+epuletek_lista = epuletek.beolvasas()
+m = epuletek.magas_epuletek_szama(epuletek_lista)
+legoregebb = epuletek.legoregebb_epulet_orszaga(epuletek_lista)
+(epuletek_lista, m, legoregebb)
